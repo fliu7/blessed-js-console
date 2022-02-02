be09e143ebcd6a67f19247a6d1238b04fff8075e
Frank Liu <fliu@asana.com>
2022-02-02 09:58:42 -0800

    Remove spinners, indicator, hex view

--- start.js
+++ start.js
@@ -1,39 +1,11 @@
-var Janeway = require('./index.js');
+var JS2 = require('./dist/js2.js');
 
-Janeway.start(function onReady(err) {
+JS2.start(function onReady(err) {
 
-	var spinner,
-	    id,
-	    i = 0;
-
-	console.log('At ease, ensign. Before you sprain something.');
-
-	Janeway.setTitle('Janeway\'s terminal title');
-	Janeway.setStatus('This is the statusbar');
-
-	// Some crappy code just to show how status setText works
-	id = setInterval(function spinnerInterval() {
-
-		var spintext;
-
-		if (!spinner) {
-			spinner = Janeway.setStatus();
-		}
-
-		i += 10;
-
-		spintext = Janeway.Blast.Bound.String.multiply('*', Math.ceil(i/10));
-		spintext += Janeway.Blast.Bound.String.multiply(' ', Math.ceil((100-i)/10));
-
-		spinner.setText(spintext);
-
-		if (i == 100) {
-			clearInterval(id);
-		}
-	}, 500);
+	JS2.setTitle('JS2');
 });
 
 process.on('uncaughtException', function onError(error) {
-	Janeway.debug('Found error...');
-	Janeway.debug(error);
+	JS2.debug('Found error...');
+	JS2.debug(error);
 });
