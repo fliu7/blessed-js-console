5a989cc69a1298b53bb05cd6ebd061e1270b21e1
Frank Liu <fliu@asana.com>
2022-02-09 11:01:44 -0800

    Support custom eval, remove menu

--- start.js
+++ start.js
@@ -1,11 +1,40 @@
 var JsConsole = require('./index.js');
 
-JsConsole.start(function onReady(err) {
-
-	JsConsole.setTitle('JsConsole');
-});
+JsConsole.start();
 
 process.on('uncaughtException', function onError(error) {
-	JsConsole.debug('Found error...');
-	JsConsole.debug(error);
+    JsConsole.debug(error);
 });
+
+/**
+ * Example: 
+ *
+ * Use a custom evaluate function and run script inside a fiber.
+ * This simulates Luna server environment. 
+ */
+
+/*
+var Fiber = require('fibers');
+function init() {
+    global.foo = 'bar';
+}
+
+let vmFiber;
+Fiber(function() {
+    vmFiber = Fiber.current;
+    init();
+    Fiber.yield();
+    var vm = require('vm');
+    while (vmFiber.code) {
+        const script = new vm.Script(vmFiber.code,  { filename: 'evaluate.vm' });
+        const result = script.runInThisContext();
+        vmFiber.code = null;
+        Fiber.yield(result);
+    }
+}).run();
+
+JsConsole.start(function evaluate(code) {
+    vmFiber.code = code;
+    return vmFiber.run();
+});
+*/
